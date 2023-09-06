4490a69adf74d2d3a97db521c8cc957df78ebd55
Preethi Tapati <118854537+PreethiTapati@users.noreply.github.com>
2023-09-06 12:43:16 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
 const data = {
     // baseUrl: 'http://localhost:5000/movies'
-    baseUrl:
-        'https://my-json-server.typicode.com/PreethiTapati/movies-list/movies'
+    baseUrl: 'https://my-json-server.typicode.com/PreethiTapati/movies-list/movies'
 };
 export default data;
