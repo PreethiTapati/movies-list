c950ec4d3e0dab53f5d0d4773dc37dd6bfd23e9c
PreethiTapati <preetimadhuri22@gmail.com>
2023-09-06 15:48:21 +0200

    added some styles

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const data = {
-     //baseUrl: 'http://localhost:5000/movies'
-   
      //
-    baseUrl: 'https://my-json-server.typicode.com/PreethiTapati/movies-list/movies'
+     baseUrl: 'http://localhost:5000/movies'
+   
+     //baseUrl: 'https://my-json-server.typicode.com/PreethiTapati/movies-list/movies'
 };
 export default data;
