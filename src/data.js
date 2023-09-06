ccc0ddafd60fdd159923776c27ceb99d49936ab8
PreethiTapati <preetimadhuri22@gmail.com>
2023-09-06 14:02:15 +0200

    add url

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 const data = {
-     baseUrl: 'http://localhost:5000/movies'
-   // baseUrl: 'https://my-json-server.typicode.com/PreethiTapati/movies-list/movies'
+     //baseUrl: 'http://localhost:5000/movies'
+   
+     baseUrl: 'https://my-json-server.typicode.com/PreethiTapati/movies-list/movies'
 };
 export default data;
